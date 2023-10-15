f56e51cba2831e28e78ab4bb50e6963f9791f1d9
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-15 16:55:28 +0530

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const links = document.querySelectorAll('li a')
 const slideContainer = document.querySelector('.hero--slider-container')
 const poularMoviesCardsContainer = document.querySelector('.popular-movies .cards-container')
 const topRatedMoviesCardsContainer = document.querySelector('.top-rated-movies .cards-container')
-const movieInfoContainer = document.querySelector('.movie-info-container')
+const main = document.querySelector('main')
 const API_KEY = '3fd2be6f0c70a2a598f084ddfb75487c'
 const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=3fd2be6f0c70a2a598f084ddfb75487c'
 const POPULAR_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
@@ -10,74 +10,12 @@ const TOP_RATED_URL = 'https://api.themoviedb.org/3/movie/top_rated?language=en-
 const SEARCH_API_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query='
 
 
-// let myMovies;
-/*
-getMovies(API_URL)
-function getMovies(url) {
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            document.querySelector('main').innerHTML = ''
-            data['results'].forEach(element => {
- 
-                const movieCard = document.createElement('div')
-                movieCard.classList.add('movie--card')
-                movieCard.innerHTML = `<div class="image">
-                <img src="https://image.tmdb.org/t/p/w1280${element.poster_path}" alt="">
-            </div>
-            <h2 class="movie--title">${element.title}</h2>
-            <button class="movie--add">Add</button>
-            <span class="movie--rating ${classByRating(+element.vote_average)}">${element.vote_average}</span>
-            <p class="movie--overview">${element.overview}
-            </p>
-        </div>`
- 
-                document.querySelector('main').appendChild(movieCard)
- 
- 
- 
-            });
-        }
- 
- 
-        )
-        .then(data => {
-            let add = document.querySelectorAll('.movie--add')
-            add.forEach(btn => {
-                btn.addEventListener('click',
-                    () => {
-                        let movie = btn.previousElementSibling.innerText
-                        movie = localStorage.getItem('movies') + ';' + movie
-                        localStorage.setItem('movies', movie)
-                    }
-                )
-            })
-        })
-        .catch(err => { document.querySelector('main').innerHTML = `<h1>${err}</h1>` })
-}
- 
-function classByRating(rating) {
-    if (rating > 8) { return 'green' }
-    if (rating > 6) { return 'yellow' }
-    if (rating > 4) { return 'orange' }
-    else { return 'red' }
-}
- 
-document.querySelector('button').addEventListener('click', () => {
-    const query = document.querySelector('input').value
-    getMovies(SEARCH_API_URL + query)
-})
- 
-links.forEach(link => link.addEventListener('click', () => { location.reload() }))
- */
-
-
 fetch(NOW_PLAYING_URL)
     .then(data => data.json())
     .then(data => {
         console.log(data)
-        slideContainer.innerHTML = ''
+        try { slideContainer.innerHTML = '' }
+        catch (err) { console.log(err) }
         data['results'].forEach(element => {
             const slide = document.createElement('div')
             slide.classList.add('slide', 'active')
@@ -90,22 +28,41 @@ fetch(NOW_PLAYING_URL)
 
         </i> <span class ="vote-average">${element['vote_average']}/10</span>
     </div>`
-            slideContainer.appendChild(slide)
+            try {
+                slideContainer.appendChild(slide)
+            }
+            catch (err) {
+                console.log(err)
+            }
         });
         const slides = document.querySelectorAll('.slide')
         updateSlide(slides)
     })
+    .catch(err => console.log(err))
 
 function createCards(url, parent) {
     fetch(url)
         .then(data => data.json())
         .then(data => {
-            parent.innerHTML = ''
+            try {
+                parent.innerHTML = ''
+            }
+            catch (err) {
+                console.log(err)
+            }
             data['results'].forEach(elm => {
                 const card = document.createElement('div')
                 card.classList.add('movie-card')
                 card.setAttribute('data-movieID', `${elm.id}`)
-                card.addEventListener('click', () => { movieInfo(card) })
+                card.addEventListener('click', (e) => {
+                    const movieInfoContainer = document.querySelector('.movie-info-container')
+                    console.log(movieInfoContainer)
+                    const movieID = card.getAttribute('data-movieid')
+                    console.warn(e.target)
+                    console.warn(movieID)
+                    const ID_API_URL = `https://api.themoviedb.org/3/movie/${movieID}?api_key=3fd2be6f0c70a2a598f084ddfb75487c`
+                    movieInfo(ID_API_URL)
+                })
                 card.innerHTML = `<div class="card--movie-image">
             <img src="https://image.tmdb.org/t/p/w1280${elm.poster_path}" alt="">
             </div>
@@ -115,56 +72,52 @@ ${elm.title}
             <div class="card--movie-time">
 ${elm["release_date"]}
             </div>`
-                parent.appendChild(card)
+                try { parent.appendChild(card) }
+                catch (err) { console.log(err) }
             })
         })
+        .catch(err => console.log(err))
 }
 
-function updateSlide(array) {
-    let count = 0
-    setInterval(() => {
-        array.forEach(ele => ele.classList.remove('active'))
-        array[count].classList.add('active')
-        count++
-        if (count == (array.length - 1)) {
-            count = 0
-        }
-    }, 3000)
-}
+// function updateSlide(array) {
+//     let count = 0
+//     setInterval(() => {
+//         array.forEach(ele => ele.classList.remove('active'))
+//         array[count].classList.add('active')
+//         count++
+//         if (count == (array.length - 1)) {
+//             count = 0
+//         }
+//     }, 3000)
+// }
 
-function movieInfo(card) {
-    const movieID = card.getAttribute('data-movieID')
-    console.log(movieID)
-    const ID_API_URL = `https://api.themoviedb.org/3/movie/${movieID}?api_key=${API_KEY}`
-    window.location = '../movieInfo.html'
-    fetch(ID_API_URL)
+function movieInfo(url) {
+    fetch(url)
         .then(data => data.json())
         .then(data => {
             console.log(data)
-            movieInfoContainer.innerHTML = ''
-            movieInfoContainer.innerHTML = `<div class="movie-info">
+            main.innerHTML = ''
+            main.className = 'movie-info-container container'
+            main.innerHTML = `<div class="movie-info">
 <div class="movie-info-image">
-<img src="https://image.tmdb.org/t/p/w1280${poster_path}">
+<img src="https://image.tmdb.org/t/p/w1280${data.poster_path}">
 </div>
 <div class="movie-info-content">
-    <div class="movie-info-name">
-        ${data.original_title}
-    </div>
-    <div class="movie-info-rating">
-        <i class="fa-solid fa-star"></i>${data.vote_average}/vote_average
-    </div>
-    <div class="movie-info-release">
-        ${data.release_date}
-    </div>
-    <div class="movie-info-overview">
-       ${data.overview}
-    </div>
-    <ul class="movie-info-genres"> GENRES
-        <li class="movie-info-genre-item">lorem</li>
-        <li class="movie-info-genre-item">lorem</li>
-        <li class="movie-info-genre-item">lorem</li>
-    </ul>
-    <a href="" class="movie-info-homepage-link">link homepage</a>
+<div class="movie-info-name">
+${data.original_title}
+</div>
+<div class="movie-info-rating">
+<i class="fa-solid fa-star"></i>${data.vote_average}/10
+</div>
+<div class="movie-info-release">
+${data.release_date}
+</div>
+<div class="movie-info-overview">
+${data.overview}
+</div>
+<ul class="movie-info-genres"> GENRES
+</ul>
+<a href="${data.homepage}" class="movie-info-homepage-link">link to homepage</a>
 </div>
 
 </div>
@@ -172,17 +125,39 @@ function movieInfo(card) {
 MOVIE INFO
 </div>
 <ul class="movie-info-additional">
-<li class="movie-info-additional-item"><span>BUDGET</span></li>
-<li class="movie-info-additional-item"><span>RUNTIME</span></li>
-<li class="movie-info-additional-item"><span>REVENUE</span></li>
-<li class="movie-info-additional-item"><span>STATUS</span></li>
+<li class="movie-info-additional-item"><span>BUDGET:</span>${data.budget}</li>
+<li class="movie-info-additional-item"><span>RUNTIME:</span>${data.runtime} mins</li>
+<li class="movie-info-additional-item"><span>REVENUE:</span>${data.revenue}</li>
+<li class="movie-info-additional-item"><span>STATUS:</span>${data.status}</li>
 </ul>
-<div class="movie-info-production">
+<ul class="movie-info-production">
 <h3>PRODUCTION COMAPANIES</h3>
-<span>Lorem ipsum dolor sit amet consectetur adipisicing.</span>
+</ul>
 </div>
 </div>`
+
+            const genreContainer = document.querySelector('.movie-info-genres')
+            console.log(data)
+            data.genres.forEach(genre => {
+                console.log(genre)
+                const li = document.createElement('li')
+                li.className = 'movie-info-genre-item'
+                li.innerText = genre['name']
+                genreContainer.appendChild(li)
+
+                const productionComapaniesContainer = document.querySelector('.movie-info-production')
+                data.production_companies.forEach(company => {
+                    console.log(genre)
+                    const li = document.createElement('li')
+                    li.className = 'movie-info-company-item'
+                    li.innerText = company['name']
+                    productionComapaniesContainer.appendChild(li)
+                    li.style.display = 'inline-block'
+                })
+            })
         })
+
+
 }
 
 createCards(NOW_PLAYING_URL, poularMoviesCardsContainer)
