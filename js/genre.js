f254dd804e1eb8f56b653866d63d5062754dc456
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-23 19:47:00 +0530

    added background images

--- js/genre.js
+++ js/genre.js
@@ -9,7 +9,27 @@ genres.style.marginTop = document.querySelector('header').offsetHeight + 'px'
 genre.style.marginTop = document.querySelector('header').offsetHeight + 'px'
 const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
 const MAIN_URL = `https://api.themoviedb.org/3/discover/movie?with_genres=`
-
+const images = {
+    "Action": 'images/Action.jpg',
+    "Adventure": 'images/Adventure.jpeg',
+    "Animation": 'images/Animation.jpeg',
+    "Comedy": 'images/Comedy.jpg',
+    "Horror": 'images/Horror.jpg',
+    "Crime": 'images/Crime.jpg',
+    "Documentary": 'images/Documentary.jpg',
+    "Drama": 'images/Drama.jpeg',
+    "Family": 'images/Family.jpg',
+    "Science Fiction": 'images/Science-Fiction.jpg',
+    "History": 'images/History.jpeg',
+    "War": 'images/war.jpeg',
+    "Romance": 'images/Romance.jpg',
+    "Thriller": "images/Thriller.jpg",
+    "Music": "images/Music.webp",
+    "Mystery": "images/Mystery.jpg",
+    "Fantasy": "images/Fantasy.jpg",
+    "Western": "images/Western.avif",
+    "TV Movie": "images/TV-movie.jpg"
+}
 
 async function createGenreCards(url) {
     const res = await fetch(url)
@@ -18,6 +38,8 @@ async function createGenreCards(url) {
     await data.forEach(element => {
         const card = document.createElement('div')
         card.classList.add('genre-card')
+        // images[element.name] = `images/${element.name}.jpeg`
+        card.style.backgroundImage = `url('${images[element.name]}')`;
         card.innerHTML = `<h3>${element.name}</h3>`
         genreNameCardsContainer.appendChild(card)
         card.setAttribute('id', `${element.id}`)
